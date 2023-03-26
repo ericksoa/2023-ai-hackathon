6c4065b48bbe6284bc62f1c0f9a1f4864ad46005
aaron <aaron@orgspace.io>
2023-03-26 10:40:40 -0700

    last ui update

--- completions-endpoint/pages/index.tsx
+++ completions-endpoint/pages/index.tsx
@@ -1,6 +1,7 @@
 // components/HomePage.tsx
 import React, { useState } from 'react';
-import Codeblock from "@/pages/codeblock";
+import Codeblock from '@/pages/codeblock';
+import styles from './HomePage.module.css';
 
 interface DataSource {
   id: string;
@@ -8,19 +9,20 @@ interface DataSource {
 }
 
 const dataSources: DataSource[] = [
-  { id: 'covid-data', name: "Covid Data" },
-  { id: 'political-contributions' , name: "Political Contribution Data"},
-  { id: 'my-company-data', name: "My Company Data" },
-]
+  { id: 'covid-data', name: 'Covid Data' },
+  { id: 'political-contributions', name: 'Political Contribution Data' },
+  { id: 'my-company-data', name: 'My Company Data' },
+];
+
 const HomePage: React.FC = () => {
-  const [query, setQuery] = useState('')
-  const [dataSource, setDataSource] = useState(dataSources[0].id)
-  const [isLoading, setIsLoading] = useState(false)
-  const [answer, setAnswer] = useState('')
-  const [promptAsk, setPromptAsk] = useState('')
-  const [promptStructure, setPromptStructure] = useState('')
-  const [promptConditions, setPromptConditions] = useState('')
-  const [computeFunction, setComputeFunction] = useState('')
+  const [query, setQuery] = useState('');
+  const [dataSource, setDataSource] = useState(dataSources[0].id);
+  const [isLoading, setIsLoading] = useState(false);
+  const [answer, setAnswer] = useState('');
+  const [promptAsk, setPromptAsk] = useState('');
+  const [promptStructure, setPromptStructure] = useState('');
+  const [promptConditions, setPromptConditions] = useState('');
+  const [computeFunction, setComputeFunction] = useState('');
 
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setDataSource(e.target.value);
@@ -40,30 +42,35 @@ const HomePage: React.FC = () => {
     });
 
     const result = await response.json();
-    setAnswer(result.answer)
-    setPromptAsk(result.promptAsk)
-    setPromptStructure(result.promptStructure)
-    setPromptConditions(result.promptConditions)
-    setComputeFunction(result.computeFunction)
+    setAnswer(result.answer);
+    setPromptAsk(result.promptAsk);
+    setPromptStructure(result.promptStructure);
+    setPromptConditions(result.promptConditions);
+    setComputeFunction(result.computeFunction);
     setIsLoading(false);
   };
 
   return (
-    <div className="container">
-      <h1>Your Own Private GPT</h1>
-      <h4>Want to have OpenAI help you search data, without handing your data over? Today we show how to give it enough context to solve the problem without handing over all the data. This is critical for apps like HR</h4>
-      <div className="search-box">
+    <div className={styles.pageWrapper}>
+      <div className={styles.container}>
+      <h1 className={styles.title}>Private GPT</h1>
+      <h4 className={styles.subtitle}>
+        Want to have OpenAI help you search data, without handing your data over to the AI Engine? Today we show how
+        to give it enough context to solve the problem without handing over all the data. This is
+        critical for apps like HR
+      </h4>
+      <div className={styles.searchBox}>
         <input
           type="text"
           placeholder="Type your question here..."
           value={query}
           onChange={(e) => setQuery(e.target.value)}
-          className="query-input"
+          className={styles.queryInput}
         />
-        <button onClick={handleAsk} disabled={isLoading} className="ask-button">
+        <button onClick={handleAsk} disabled={isLoading} className={styles.askButton}>
           Ask
         </button>
-        <select value={dataSource} onChange={handleChange} className="data-source">
+        <select value={dataSource} onChange={handleChange} className={styles.dataSource}>
           {dataSources.map((source) => (
             <option key={source.id} value={source.id}>
               {source.name}
@@ -71,95 +78,30 @@ const HomePage: React.FC = () => {
           ))}
         </select>
       </div>
-      {isLoading && <div className="loading">Thinking...</div>}
-      {answer && (
-        <div className="answer-section">
-          <h2>Answer:</h2>
-          <div className="answer-box">{answer}</div>
+      {isLoading && <div className={styles.loading}>Thinking...</div>}
+      <div className={styles.answerSection}>
+        <div className={styles.results}>
+          {answer && (
+            <div className={styles.resultCard}>
+              <h2 className={styles.sectionTitle}>Answer:</h2>
+              <div className={styles.answerBox}>{answer}</div>
+            </div>
+          )}
+          {promptStructure && (
+            <div className={styles.resultCard}>
+              <h2 className={styles.sectionTitle}>Inferred data structure:</h2>
+              <Codeblock code={promptStructure} />
+            </div>
+          )}
+          {computeFunction && (
+            <div className={styles.resultCard}>
+              <h2 className={styles.sectionTitle}>Compute Function:</h2>
+            <Codeblock code={computeFunction} />
+            </div>
+            )}
         </div>
-      )}
-      {promptStructure && (
-        <div className="answer-section">
-          <h2>Inferred data structure:</h2>
-          <Codeblock code={promptStructure} />
-        </div>
-      )}
-      {computeFunction && (
-        <div className="answer-section">
-          <h2>Compute Function:</h2>
-          <Codeblock code={computeFunction} />
-        </div>
-      )}
-      <style jsx>{`
-        .container {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          gap: 16px;
-          justify-content: center;
-          font-family: Arial, sans-serif;
-        }
-        h1 {
-          font-size: 24px;
-          margin-bottom: 40px;
-        }
-        h4 {
-          font-size: 10px;
-          margin-bottom: 3px;
-        }
-        .search-box {
-          display: flex;
-          flex-direction: column;
-          gap: 8px;
-          align-items: center;
-        }
-        .query-input {
-          width: 600px;
-          padding: 12px 20px;
-          border: 1px solid #ccc;
-          border-radius: 4px;
-        }
-        .ask-button {
-          background-color: #4caf50;
-          color: white;
-          font-size: 14px;
-          border: none;
-          border-radius: 4px;
-          cursor: pointer;
-          padding: 12px 20px;
-        }
-        .ask-button:hover {
-          background-color: #45a049;
-        }
-        .data-source {
-          width: 200px;
-          padding: 8px;
-          border: 1px solid #ccc;
-          border-radius: 4px;
-        }
-        .loading {
-          font-size: 14px;
-    }
-    .answer-section {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      gap: 8px;
-      width: 100%;
-    }
-    h2 {
-      font-size: 18px;
-    }
-    .answer-box {
-      border: 1px solid #ccc;
-      padding: 16px;
-      min-height: 100px;
-      width: 100%;
-      max-width: 600px;
-      text-align: left;
-      word-wrap: break-word;
-    }
-  `}</style>
+      </div>
+    </div>
     </div>
   );
 };
